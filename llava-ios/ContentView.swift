8ef4fc7a35020928f1a63ec843222c57502bb6d7
Prashanth Sadasivan <prashanthsadasivan@gmail.com>
2024-02-02 16:55:12 -0500

    Initial Commit

---
+++ llava-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  llava-ios
+//
+//  Created by Prashanth Sadasivan on 2/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ llava-ios/llava_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  llava_iosApp.swift
+//  llava-ios
+//
+//  Created by Prashanth Sadasivan on 2/2/24.
+//
+
+import SwiftUI
+
+@main
+struct llava_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
